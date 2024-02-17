883d565a91219deb5cc9ef8ec9e344df32c4dcc1
siv-0821 <sivasankar11237@gmail.com>
2024-02-17 14:30:50 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,29 @@ import LoginForm from "./component/login/login";
 import SignupForm from "./component/signup/signup";
 import Dashboard from "./component/assets/js/dashboard";
 import Forgetpassword from "./component/forget/forgetpassword";
-import otp from "./component/forget/otp";
+import Otp from "./component/forget/otp";
 import Resetpassword from "./component/forget/resetpassword";
 import CustomCard from "./component/assets/js/card";
 import Sidebar from "./component/Dashboard/sidenavebar";
 import Productupload from "./component/products/proupload";
-function App()
-{
-  return(
+function App() {
+  return (
     <>
-    <Routes>
-      <Route path="/" Component={LoginForm}/> 
-      <Route path="/signup" Component={SignupForm}/>
-      <Route path="/dashboard" Component={Dashboard}/>
-      <Route path="/forgetpassword" Component={Forgetpassword}/>
-      <Route path="/otp" Component={otp}/>
-      <Route path='/resetpassword' Component={Resetpassword}/>
-      <Route path="/card" Component={CustomCard}/> 
-      <Route path="/sidenavebar" Component={Sidebar}/>
-      <Route path="/proupload" Component={Productupload}/>
-    </Routes>
+      <Routes>
+        <Route path="/" Component={LoginForm} />
+        <Route path="/signup" Component={SignupForm} />
+        <Route path="/dashboard" Component={Dashboard} />
+        <Route path="/forgetpassword" Component={Forgetpassword} />
+        <Route path="/otp" Component={Otp} />
+        <Route path='/resetpassword' Component={Resetpassword} />
+        <Route path="/card" Component={CustomCard} />
+        <Route path="/sidenavebar" Component={Sidebar} />
+        <Route path="/proupload" Component={Productupload} />
+      </Routes>
     </>
   )
 }
-export default App    
+export default App
 
 /* import React from 'react';
 import Index from './component/pages';
@@ -35,7 +34,6 @@ function App() {
   return (
     <div>
       <Index/>
-      
     </div>
   );
 }
