ff5b566a7da169168ced54be45df70fd654eec63
Aravind <147846043+s-aravind-bca@users.noreply.github.com>
2024-03-28 20:45:35 +0530

    changes

--- Controller/purchaseController.js
+++ Controller/purchaseController.js
@@ -1,25 +1,28 @@
-import Purchase from '../Models/purchase.js'
+import Purchase from '../Models/purchase.js';
 
 const purchaseProduct = async (req, res) => {
   try {
-    const { customerID, customerName, productID, productName, paymentMode,amount } = req.body
-    console.log(customerID, customerName, productID, productName,  paymentMode,amount)
+    const { customerID, name, doorNo, area, landmark, city, mobileNumber, pinCode, orderID, paymentID, productName, productModel,quantity, amount } = req.body;
+    
+    // Validate required fields
+    if (!customerID || !name || !doorNo || !area || !landmark || !city || !mobileNumber || !pinCode || !orderID || !paymentID || !productName || !productModel || !quantity || !amount) {
+      return res.status(400).json({ message: "All required are required" });
+    }
+
     const newPurchase = new Purchase({
-      customerID,
-      customerName,
-      productID,
-      productName,
-     paymentMode,
-     amount
-    })
-    //Create new purchase record
-    const savedPurchase = await newPurchase.save()
-    res.status(201).json(savedPurchase)
-    console.log('Purchase Created Successfully')
+      customerID, name, doorNo, area, landmark, city, mobileNumber, pinCode, orderID, paymentID, productName, quantity, amount
+    });
+
+    // Save the new purchase record
+    const savedPurchase = await newPurchase.save();
+    res.status(201).json(savedPurchase);
+    console.log('Ordered Successfully!');
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    res.status(400).json({ message: error.message });
   }
-}
+};
+
+
 const getAllPurchases = async (req, res) => {
   try {
     const allPurchases = await Purchase.find();
@@ -33,7 +36,7 @@ const getPurchaseById = async (req, res) => {
   try {
     const purchase = await Purchase.findById(req.params.id)
     if (!purchase) {
-      return res.status(404).json({ message: 'Purchase not found' })
+      return res.status(404).json({ message: 'Order not found' })
     }
     res.json(purchase)
   } catch (error) {
@@ -46,10 +49,10 @@ const updatePurchase = async (req, res) => {
     const updatedPurchase = await Purchase.findByIdAndUpdate(req.params.id, req.body, { new: true })
 
     if (!updatedPurchase) {
-      return res.status(404).json({ message: 'Purchase not found' })
+      return res.status(404).json({ message: 'Order not found' })
     }
     res.json(updatedPurchase)
-    console.log('Purchase Updated Successfully')
+    console.log('Order Updated Successfully')
   } catch (error) {
     res.status(400).json({ message: error.message })
   }
@@ -59,9 +62,9 @@ const deletePurchase = async (req, res) => {
   try {
     const deletedPurchase = await Purchase.findByIdAndDelete(req.params.id)
     if (!deletedPurchase) {
-      return res.status(404).json({ message: 'Purchase not found' })
+      return res.status(404).json({ message: 'Order not found' })
     }
-    res.json({ message: 'Purchase deleted successfully' })
+    res.json({ message: 'Order deleted successfully' })
   } catch (error) {
     res.status(400).json({ message: error.message })
   }
--- Models/purchase.js
+++ Models/purchase.js
@@ -1,16 +1,23 @@
-// models/purchase.js
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const purchaseSchema = new mongoose.Schema({
   customerID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  customerName: { type: String, required: true },
-  productID: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  name: { type: String, required: true },
+  doorNo: { type: String, required: true },
+  area: { type: String, required: true },
+  landmark: { type: String, required: true },
+  city: { type: String, required: true },
+  mobileNumber: { type: String, required: true },
+  pinCode: { type: Number, required: true },
+  orderID: { type: String, required: true },
+  paymentID: { type: String, required: true },
   productName: { type: String, required: true },
-  paymentMode :{type: String},
-  amount : {type: Number},
-  purchaseDate: { type: Date, default: Date.now },
-})
+  productModel:{type:String,require:true},
+  quantity: { type: String, required: true },
+  amount: { type: Number, required: true },
+  purchaseDate: { type: Date, default: Date.now }
+});
 
-const Purchase = mongoose.model('Purchase', purchaseSchema)
+const Purchase = mongoose.model('Purchase', purchaseSchema);
 
-export default Purchase
+export default Purchase;
